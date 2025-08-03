c329da4cb3b10ccf3bf6968c98b17ab705f9728e
SanjayaPrasadRajapaksha <sanjayaprasad823@gmail.com>
2025-08-03 11:58:39 +0530

    Add repositories

---
+++ hotel-management-service-api/src/main/java/com/cpd/hotel_system/hotel_management_service_api/repo/RoomRepo.java
@@ -0,0 +1,6 @@
+package com.cpd.hotel_system.hotel_management_service_api.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepo extends JpaRepository <RoomRepo, String> {
+}
